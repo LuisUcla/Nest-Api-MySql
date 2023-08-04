3e9e6d2d0d20e8c6136ff80db81f8f8fc0d6cfec
LuisUcla <caluigi7@gmail.com>
2023-08-04 09:50:07 -0400

    refactor: change method create cat, add logic for add a breed

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,31 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 import { Cat } from './entities/cat.entity';
 import { Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
+import { Breed } from '../breeds/entities/breed.entity';
 
 @Injectable()
 export class CatsService {
   constructor( 
     @InjectRepository(Cat)
-    private readonly catsRepository: Repository<Cat> // para usar los metodos de la DB MySql
+    private readonly catsRepository: Repository<Cat>, // para usar los metodos de la DB MySql
+
+    @InjectRepository(Breed)
+    private readonly breedRepository: Repository<Breed>
   ) {}
 
 
   async create(createCatDto: CreateCatDto) {
-    return await this.catsRepository.save(createCatDto);
+    const breed = await this.breedRepository.findOne({ })
+
+    if (!breed) {
+      throw new BadRequestException('Breed not found')
+    }
+    
+    return await this.catsRepository.save({
+      ...createCatDto,
+      breed
+    });
+    // return
   }
 
   async findAll() {
-    return await this.catsRepository.find();
+    return await this.catsRepository.find({});
   }
 
   async findOne(id: number) {
     return await this.catsRepository.findOneBy({id});
   }
 
-  async update(id: number, updateCatDto: UpdateCatDto) {}
+  async update(id: number, updateCatDto: UpdateCatDto) {
+   // return await this.catsRepository.update(id, updateCatDto)
+    return
+  }
 
   async remove(id: number) {
     // return await this.catsRepository.softDelete( id ) // elimina de forma logica
