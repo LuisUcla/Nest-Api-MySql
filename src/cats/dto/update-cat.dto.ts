c78f3ef5f3b2c402c8d7b5d811f42bf7a478093a
LuisUcla <caluigi7@gmail.com>
2023-08-04 09:56:41 -0400

    feat: add update-cat-dto for update validation

--- src/cats/dto/update-cat.dto.ts
+++ src/cats/dto/update-cat.dto.ts
@@ -1,6 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCatDto } from './create-cat.dto';
+import { IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, MinLength } from 'class-validator';
 
 export class UpdateCatDto extends PartialType(CreateCatDto) {
-    
+    @IsString()
+    @MinLength(3)
+    @IsNotEmpty() // no puede ser vacio
+    @IsOptional() 
+    name?: string
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    age?: number
+
+    @IsString()
+    @IsOptional()
+    breed?: string
 }
