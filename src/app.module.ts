cb6e95d477801adfe0840e660581f5f0c1bd18f7
LuisUcla <caluigi7@gmail.com>
2023-08-04 09:57:21 -0400

    ci: add BreedsModule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 import { dbConnect } from './config/mysql';
+import { BreedsModule } from './breeds/breeds.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dbConnect), // --> DB conection
-    CatsModule,
+    CatsModule, BreedsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
