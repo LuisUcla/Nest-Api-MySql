0ff4111619e5687333c85ac706e8f819313771b1
LuisUcla <caluigi7@gmail.com>
2023-08-07 09:05:09 -0400

    feat: add new resource user

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column({ unique: true, nullable: false }) // nullable es que no puede ser vacio
+    email: string
+    
+    @Column({ nullable: false }) // nullable es que no puede ser vacio
+    password: string
+
+    // @Column({ default: 'user' })
+    // rol: []
+
+    @DeleteDateColumn()
+    deletedAt: Date
+}
