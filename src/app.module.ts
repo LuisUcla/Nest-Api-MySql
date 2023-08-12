e8fd170c733335d538d8570e973fb9069169c07f
LuisUcla <caluigi7@gmail.com>
2023-08-12 15:43:56 -0400

    refactor: add userModule and AuthModule in app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 import { dbConnect } from './config/mysql';
 import { BreedsModule } from './breeds/breeds.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dbConnect), // --> DB conection
-    CatsModule, BreedsModule,
+    CatsModule, BreedsModule, UsersModule, AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
