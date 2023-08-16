ab99a9bb97be3d03de1b86bec293918161901add
LuisUcla <caluigi7@gmail.com>
2023-08-16 12:26:02 -0400

    refactor: change o delete validaciones
    Se elimina porque no es necesario

--- src/cats/dto/update-cat.dto.ts
+++ src/cats/dto/update-cat.dto.ts
@@ -3,18 +3,18 @@ import { CreateCatDto } from './create-cat.dto';
 import { IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, MinLength } from 'class-validator';
 
 export class UpdateCatDto extends PartialType(CreateCatDto) {
-    @IsString()
-    @MinLength(3)
-    @IsNotEmpty() // no puede ser vacio
-    @IsOptional() 
-    name?: string
+    // @IsString()
+    // @MinLength(3)
+    // @IsNotEmpty() // no puede ser vacio
+    // @IsOptional() 
+    // name?: string
 
-    @IsInt()
-    @IsPositive()
-    @IsOptional()
-    age?: number
+    // @IsInt()
+    // @IsPositive()
+    // @IsOptional()
+    // age?: number
 
-    @IsString()
-    @IsOptional()
-    breed?: string
+    // @IsString()
+    // @IsOptional()
+    // breed?: string
 }
