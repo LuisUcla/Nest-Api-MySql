fb84172994bcb8134f6d2649399ccd962e008543
LuisUcla <caluigi7@gmail.com>
2023-08-16 09:11:24 -0400

    ci: integrates routes import

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,4 +1,4 @@
-import { Role } from '../enums/rol.enum';
+import { Role } from '../../common/enums/rol.enum';
 import { AuthGuard } from '../guard/auth.guard';
 import { RolesGuard } from '../guard/roles.guard';
 
@@ -7,7 +7,7 @@ import { Roles } from './roles.decorator';
 
 export function Auth(role: Role) {
     return applyDecorators( // aplica varios decoradores en uno
-        Roles(role), // middleware de verificar el role
+        Roles(role), // para guardar datos en la metadata
         UseGuards(AuthGuard, RolesGuard) // middleware de autenticacion y de roles
     )
 }
