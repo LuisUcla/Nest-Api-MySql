7248a66ec8a8ff3b02988dffdb4f94a64f80182a
LuisUcla <caluigi7@gmail.com>
2023-08-16 09:15:39 -0400

    feat: add relation with table user

--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,5 +1,6 @@
+import { User } from '../../users/entities/user.entity';
 import { Breed } from '../../breeds/entities/breed.entity';
-import { Entity, Column, PrimaryGeneratedColumn, DeleteDateColumn, ManyToOne } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, DeleteDateColumn, ManyToOne, JoinColumn } from 'typeorm';
 
 @Entity() // decorador para dar los poderes a la clase
 export class Cat { // DB Relacional con MySql Tabla: Cat = Gato
@@ -23,4 +24,11 @@ export class Cat { // DB Relacional con MySql Tabla: Cat = Gato
         eager: true // para que se traiga la raza en el findOne
     }) 
     breed: Breed;
+
+    @ManyToOne(() => User) // relacion con usuario para cuando se crea un nuevo gato
+    @JoinColumn({ name: 'userEmail', referencedColumnName: 'email' })
+    user: User;
+
+    @Column() // columna para hacer la referencia a la tabla 'user'
+    userEmail: string;
 }
