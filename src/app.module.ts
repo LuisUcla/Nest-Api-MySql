84911da8c9f6e063e1e69229c45b20edfe160f83
LuisUcla <caluigi7@gmail.com>
2023-08-21 15:36:37 -0400

    refactor: change mode of connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
-import { dbConnect } from './config/mysql';
+import { mysql_dbConnect } from './config/mysql';
 import { BreedsModule } from './breeds/breeds.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { psql_dbConnect } from './config/psql';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dbConnect), // --> DB conection
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot(process.env.TYPE === 'mysql' ? mysql_dbConnect : psql_dbConnect), // --> DB conection
     CatsModule, BreedsModule, UsersModule, AuthModule
   ],
   controllers: [AppController],
