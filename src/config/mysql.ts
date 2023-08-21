04a9dc0b1ed93fa7eb7a7d9f0d5a858d4d84487a
LuisUcla <caluigi7@gmail.com>
2023-08-21 15:35:50 -0400

    refactor: add Environment variables

--- src/config/mysql.ts
+++ src/config/mysql.ts
@@ -1,12 +1,13 @@
 import {  TypeOrmModuleOptions } from '@nestjs/typeorm';
+import 'dotenv/config'; // importacion para usar las variables de entorno
 
-export const dbConnect: TypeOrmModuleOptions = {
+export const mysql_dbConnect: TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'nest-luis',
-    password: '12345678',
-    database: 'curso-nest',
+    host: process.env.MYSQL_DATABASE_HOST,
+    port: +process.env.MYSQL_DATABASE_PORT, 
+    username: process.env.MYSQL_DATABASE_USERNAME,
+    password: process.env.MYSQL_DATABASE_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     autoLoadEntities: true,
     //entities: [],
     synchronize: true
