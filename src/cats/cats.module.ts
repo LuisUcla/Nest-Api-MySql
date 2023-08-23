afb436686b125814e32a40b39cbc5f5872e60d8a
LuisUcla <caluigi7@gmail.com>
2023-08-23 10:00:33 -0400

    refactor: add AuthModule
    En users module se hace refactor para permitir las dependencias circulares

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -4,11 +4,12 @@ import { CatsController } from './cats.controller';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Cat } from './entities/cat.entity';
 import { BreedsModule } from '../breeds/breeds.module';
+import { AuthModule } from '../auth/auth.module';
 
 // BreedsModule para usar los metodos
 // de Breed en catService
 @Module({
-  imports: [TypeOrmModule.forFeature([Cat]), BreedsModule],
+  imports: [TypeOrmModule.forFeature([Cat]), BreedsModule, AuthModule],
   controllers: [CatsController],
   providers: [CatsService]
 })
