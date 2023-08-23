f6b23b37e575e122894c785593a45115ed875c3e
LuisUcla <caluigi7@gmail.com>
2023-08-23 09:53:41 -0400

    feat: add config for sign token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, // --> para usar los metodos del servicio de user
-    JwtModule.register({
-      global: true,
-      secret: 'secret: no utitlizar en prod', // llave del token, debe estar en el .env
-      signOptions: { expiresIn: '1d' },
-    }),], 
+  imports: [
+    UsersModule, // --> para usar los metodos del servicio de user
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // llave del token, debe estar en el .env
+        global: true,
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    })], 
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [JwtModule] // se exporta para usar en los demas modulos que usan AuthModule
 })
 export class AuthModule {}
